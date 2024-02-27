9a7d17a8aeb57aa0f4ec9f48d154473e8c55082f
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-02-28 00:22:26 +0500

    practice set chap3 done

---
+++ app.js
@@ -0,0 +1,32 @@
+// Chapter3 variable for number practicing
+
+// Question 1
+var age=17;
+
+alert("I am year " + age +" old");
+
+// Question 2
+var a=15;
+
+alert("You have visited this site 14 times");
+
+// Question 3
+var birthYear=2007;
+
+document.write(" My Birthyear is " + birthYear);
+
+// Question 4 
+var companyName="www.abcwebsite.com";
+var customerName="Arham Khan";
+var productName="Shalwar Qameez";
+var quantity="2";
+
+document.write(customerName + "ordered" + quantity + productName + "from" + companyName);
+
+
+
+
+
+
+
+//Chapter3 variable for number practicing
